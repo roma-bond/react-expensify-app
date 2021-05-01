9743dd41ca95d19b862e8612b6834304bb61efaa
roma-bond <bondarenkore@gmail.com>
2021-05-01 23:25:52 +0300

    fixing a bug - expense not getting added on Heroku only

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -5,6 +5,7 @@ import { addExpense } from '../actions/expenses';
 
 export class AddExpensePage extends React.Component {
   onSubmit = (expense) => {
+    console.log(expense)
     this.props.addExpense(expense);
     this.props.history.push('/');
   };
