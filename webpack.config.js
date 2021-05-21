e0f05dac0db51d0ebb45e3d8211e9fac6f8e13df
roma-bond <bondarenkore@gmail.com>
2021-05-22 00:13:21 +0300

    Add Expensify styles

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = (env) => {
 
   return {
     mode: 'development',
-    entry: './src/app.js',
+    entry: ['babel-polyfill', './src/app.js'],
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
@@ -33,7 +33,8 @@ module.exports = (env) => {
           {
             loader: 'css-loader',
             options: {
-              sourceMap: true
+              sourceMap: true,
+              url: false
             }
           },
           {
